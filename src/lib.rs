5d8fe80f8b27f9ea84e4c7caf1e89b886f23c7fb
OssFate <mantis1512@gmail.com>
2018-12-21 21:48:21 -0500

    Added better structure Now i can start to work on the rest of the days, gotta change the name of input files imo, but yea everything is together now, lets see how good i code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,58 @@
+use std::fs::File;
+use std::io::{Read};
+
 mod day;
 
-use day::one;
+pub fn work_to_do(args: Vec<String>) {
+    let (command, file) =  get_args(args);
 
-pub fn print_s() {
-    println!("Rofl");
-    one::print_some();
-    day::two::print_some();
+    let com = match command {
+        UserInput::Command(x) => x,
+        _ => String::from("None")
+    };
+    println!("Running Command: {}", com);
+
+    let file_content = read_file(file);
+
+    match com.as_ref() {
+        "one" => day::one::do_work(file_content),
+        "two" => day::two::do_work(file_content),
+        "three" => day::three::do_work(file_content),
+        _ => println!("None option")
+    }
+
+}
+
+fn read_file(path: UserInput) -> Option<String> {
+
+    let filename = match path {
+        UserInput::File(input) => input,
+        _  => {
+                println!("Not a file!");
+                return None;
+            }
+    };
+
+    println!("Reading file: {}", filename);
+    println!("");
+    
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    
+    Some(contents)
+}
+
+fn get_args(args: Vec<String>) -> (UserInput, UserInput) {
+    let command = UserInput::Command(args[1].clone());
+    let file = UserInput::File(args[2].clone());
+    // println!("{:?}", args);
+
+    (command, file)    
+}
+
+enum UserInput {
+    Command(String),
+    File(String)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 extern crate oss_advent_code_18;
+
+use std::env;
+
 // Will have menu control and call of different functions from the lib.rs
 // need flexibility on calls
-
 // Answer: Lets just make a big match
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let filename = &args[1];
-    println!("Reading file: {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    oss_advent_code_18::work_to_do(args);
 }
